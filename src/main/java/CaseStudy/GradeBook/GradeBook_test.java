82320efdf92cfeec5fa90fa074934c9bfa2553c6
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-01 08:45:19 -0500

    Feat: shapes in OOP

--- src/main/java/CaseStudy/GradeBook/GradeBook_test.java
+++ src/main/java/CaseStudy/GradeBook/GradeBook_test.java
@@ -6,15 +6,14 @@ public class GradeBook_test {
     //main method begins program execution
     public static void main(String[] args){
         //two-dimensional array of student grades
-        int[][] gradesArray = {{87,96,70},
-                {68,87,90},
-                {78,37,93},
-                {72,87,90},
-                {78,37,90}};
-        GradeBook myGradeBook = new GradeBook(
-                "CS101 Introduction to Java Programming", gradesArray);
-        System.out.printf("Welcome to the grade book for%n%s%n%n",
-                myGradeBook.getCourseName());
-        myGradeBook.processGrades();
+//        int[][] gradesArray = {{87,96,70},
+//                {68,87,90},
+//                {78,37,93},
+//                {72,87,90},
+//                {78,37,90}};
+//        GradeBook myGradeBook = new GradeBook("CS101 Introduction to Java Programming", gradesArray);
+//        System.out.printf("Welcome to the grade book for%n%s%n%n",
+//                myGradeBook.getCourseName());
+//        myGradeBook.processGrades();
     }
 } //end class GradeBookTest
---
+++ src/main/java/Display/Shapes.java
@@ -0,0 +1,33 @@
+package Display;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Shapes extends JPanel
+{
+    //// Draws a cascade of shapes starting from the top-left corner
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+
+        //My first shapes :D !!!!!
+        for (int i = 0; i < 10; i++) {
+            g.drawRect(10 + i * 10, 10 + i * 10,
+                    50 + i * 10, 50 + i * 10);
+            g.drawOval(240 + i * 10, 10 + i * 10,
+                    50 + i * 10, 50 + i * 10);
+        }
+    }
+    public static void main (String [] args)
+    {
+        Shapes panel = new Shapes(); //creates the panel
+        JFrame application = new JFrame(); //creates a new JFrame
+
+
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        application.add(panel);
+        application.setSize(500, 290);
+        application.setVisible(true);
+
+        }
+    } // End of class shapes
