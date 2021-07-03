b26f2b9fe9cc982f950aea15a5009fee51aea40e
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-03 18:30:15 -0500

    feat: array methods

--- src/main/java/NutshellArrays.java
+++ src/main/java/NutshellArrays.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.*;3
 
 public class NutshellArrays {
 
@@ -11,4 +12,14 @@ public class NutshellArrays {
     // Arrays of objects can be sorted and searched too
     String[] strarray = new String[] {"Now", "is,","the", "time"};
     Arrays.sort(strarray);                                      //   {"is", "now", "The", "time"}
+
+
+    //Arrays.equals() compares all elements of two arrays
+    String[] clone = (String[]) strarray.clone();
+    boolean b1 = Arrays.equals(strarray.clone); // yes they're equal
+
+    // Arrays.fill() initializes array elements
+    byte[] data = new byte[100];         // An empty array; elements set to 0
+    Arrays.fill(data, (byte) -1);        // Set them all to -1
+    Arrays.fill(data, 5, 10, (byte) -2); // set elements 5,6,7,8,9, to -2
 }
