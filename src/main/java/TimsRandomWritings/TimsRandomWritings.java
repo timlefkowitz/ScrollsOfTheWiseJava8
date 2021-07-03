4156c25b846f8f0627750395c2384a7e3be4134c
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-03 17:26:52 -0500

    Feat: Brb

---
+++ src/main/java/TimsRandomWritings/TimsRandomWritings.java
@@ -0,0 +1,32 @@
+import java.util.*;
+/*
+{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}
+{}{}{}{}{}{}{}{}{}{           Hey this is a place where I will just write random Java   {}{}{}{}{}{}{}{}{}{}
+{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}
+{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}
+ */
+
+
+package TimsRandomWritings;
+
+public class TimsRandomWritings {
+
+
+    // For some Reason I like the basket idea or a backpack
+
+    String[] backpack = {"iPhone 12 Mini", "Game and Watch", "Potion of Minor Health", "3 Chains of Locks (Puzzle)",
+
+    List<String>} backpack = new backpack<String>();
+
+    public String void(backpack item)
+    {
+
+    }
+
+
+
+    public static void main (String[] args)
+    {
+
+    }
+}
