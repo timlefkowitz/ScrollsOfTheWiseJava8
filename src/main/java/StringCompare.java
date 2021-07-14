cda12a9e8ab5f466bbbc072b53763ac69c112611
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-14 11:06:11 -0500

    Feat: Comaparing strings

--- src/main/java/StringCompare.java
+++ src/main/java/StringCompare.java
@@ -1,7 +1,6 @@
 
 public class StringCompare {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         String s1 = new String("hello");
         String s2 = "goodbye";
         String s3 = "Happy Birthday";
@@ -10,22 +9,26 @@ public class StringCompare {
         System.out.printf("s1 = %s%ns2 = %s%ns3 = %s%ns4 = %s%n%n", s1, s2, s3, s4);
 
         // test for equality
-        if(s1.equals("Hello")) //true
+        if (s1.equals("Hello")) //true
             System.out.println("s1 equals \"hello\"");
         else
             System.out.println("s1 does not equal \"hello\"");
 
         //test for equality with ==
-        if(s1 == "hello") // false: they are not the same object
+        if (s1 == "hello") // false: they are not the same object
             System.out.println("s1 is the same object as \"hello\"");
         else
             System.out.println("s1 is not the same object as \"hello\"");
 
         //test for equality(ignore case)
-        if(s3.equalsIgnoreCase(s4)) // true
+        if (s3.equalsIgnoreCase(s4)) // true
             System.out.printf("%s equals %s with case ignored%n", s3, s4);
         else
             System.out.println("s3 does not equal s4");
+
+
+        // test compareTo
+        System.out.printf("%ns1.compareTo(s2) is %d", s1.compareTo(s2));
 
     }
 }
